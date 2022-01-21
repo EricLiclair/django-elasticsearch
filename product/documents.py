2b8ab1d59daee6cf3b0edcb9acc3382e05012b08
diliga8356 <diliga8356@nahetech.com>
2022-01-21 14:45:57 -0500

    initial commit

---
+++ product/documents.py
@@ -0,0 +1,13 @@
+from django_elasticsearch_dsl import Document
+from django_elasticsearch_dsl.registries import registry
+
+from .models import Product
+
+@registry.register_document
+class ProductDocument(Document):
+    class Index:
+        name = 'product'
+    
+    class Django:
+        model = Product
+        fields = ['id', 'name', 'price', 'quantity']
---
+++ product/populate.py
@@ -0,0 +1,11 @@
+from .models import Product
+import csv
+
+def populate_db():
+    with open('data.csv') as f:
+        lines = csv.reader(file)
+        for line in lines:
+            name, price, quantity = line
+            Product.objects.create(name=name, price=price, quantity=quantity)
+
+populate_db()
